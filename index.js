02b720766ccf8c6676ac72a20a6d385aa71985f7
rufaidaju <rufaidamohammadjumaa@gmail.com>
2020-04-10 01:55:11 +0200

    Adding start a new game functionality.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
-let setTimer = function(){
+let startGame = function(){
+    document.getElementById("start-game").style.display ="none";
     let seconds= document.getElementById('input').value ;
     console.log(seconds*1000);
     let presser1 = presser2 = 0 ;
@@ -11,24 +12,33 @@ let setTimer = function(){
         }else if(event.which == 115){
             presser2 += 1;
         }
-    })
+    });
+    // Add downcount
+    // let timer = seconds;
+    // while (timer!= 0){
+    //     setTimeout(function(){
+    //         document.getElementById("timer").innerHTML = timer ;
+    //         timer -=1;
+    //         console.log(timer,'timer')
+    //     },1000)
+    // }
     setTimeout(function(){
         console.log(presser1,presser2);
 
         $('#l-presser').append('<p>',presser1,'</p>');
         $('#s-presser').append('<p>',presser2,'</p>');
         if (presser1 > presser2 ){
-            $( '#l-presser' ).addClass( "the-winner" );
-            $('#l-presser').append('<p class="the-winner"> The Winner</p><img src="giphy.gif"  alt="The winner"/>');
+            $('#l-presser').append('<p class="the-winner"> The Winner</p><img class="winner-img" src="giphy.gif"  alt="The winner"/>');
         }else if(presser1 < presser2 ){
-            $('#s-presser' ).addClass( "the-winner" );
-            $('#s-presser').append('<p class="the-winner">The winner</p><img src="giphy.gif"  alt="The winner"/>');
+            $('#s-presser').append('<p class="the-winner">The winner</p><img class="winner-img" src="giphy.gif"  alt="The winner"/>');
         }else{
             $('#s-presser').append('<p> Equal result</p>');
             $('#l-presser').append('<p>Equal result</p>')
         }
+        document.getElementById("play-again").style.display ="block";
         console.log('l',presser1);
         console.log('s',presser2)
 
     },seconds*1000)
 }
+
