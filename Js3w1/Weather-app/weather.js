0601412734235d199816e32d66d4f904c78a64a5
Rufaida <eng.rufaidajumaa@gmail.com>
2020-05-05 18:17:34 +0200

    change type to submit

--- Js3w1/Weather-app/weather.js
+++ Js3w1/Weather-app/weather.js
@@ -1,22 +1,18 @@
-// Fetch data
 
 function renderOutPut(data){
     const weather = document.querySelector('#weather');
     let outPut = `The main weather in ${city} is ${data.weather[0].main} and it's ${data.weather[0].description} </br>
-              The minimum tempreture is ${data.main.temp_min} and the maximum is ${data.main.temp_max}`;
+                  The minimum tempreture is ${data.main.temp_min} and the maximum is ${data.main.temp_max}`;
     weather.innerHTML =outPut ;
 }
 
-document.querySelector('.btn').addEventListener('click',function(){
+document.addEventListener('submit',(event) => {
+    event.preventDefault();
     city=document.querySelector("#input").value;
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c6a59d89e8a69d593a4a4c996cb6ec1`)
-    .then(response => response.json())
-    .then(renderOutPut);
-    console.log('citty',city);
-});
+       .then(response => response.json())
+       .then(renderOutPut);
+})
 
-// fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c6a59d89e8a69d593a4a4c996cb6ec1`)
-//     .then(response => response.json())
-//     .then(renderOutPut);
 
 
