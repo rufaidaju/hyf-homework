fba9c679b3047e0c920b75b6cf6678ed14592289
Rufaida <eng.rufaidajumaa@gmail.com>
2020-05-07 03:16:48 +0200

    add css file

--- Js3w1/Weather-app/weather.js
+++ Js3w1/Weather-app/weather.js
@@ -1,14 +1,19 @@
 
 function renderOutPut(data){
-    const weather = document.querySelector('#weather');
-    let outPut = `The main weather in ${city} is ${data.weather[0].main} and it's ${data.weather[0].description} </br>
-                  The minimum tempreture is ${data.main.temp_min} and the maximum is ${data.main.temp_max}`;
-    weather.innerHTML =outPut ;
+    const weather = document.querySelector('.weather');
+    let minTemp = (data.main.temp_min - 273.15 ).toFixed(0); 
+    let maxTemp = (data.main.temp_max - 273.15 ).toFixed(0); 
+    
+    let weatherInfo = `<li>Temperature </br> ${minTemp}/${maxTemp}&#8451 </br>
+                  <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"></li> 
+                  <li>Wind speed<br/>${data.wind.speed}</li>
+                  <li>Cloudy <br/>${data.clouds.all} </li>`
+    weather.innerHTML =weatherInfo ;
 }
 
 document.addEventListener('submit',(event) => {
     event.preventDefault();
-    city=document.querySelector("#input").value;
+    let city=document.querySelector(".input").value;
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c6a59d89e8a69d593a4a4c996cb6ec1`)
        .then(response => response.json())
        .then(renderOutPut);
