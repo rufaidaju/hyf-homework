c44fe829034caceb8af8acb6b892b2c8706f7dda
Rufaida <eng.rufaidajumaa@gmail.com>
2020-06-06 03:17:50 +0200

    Adding convertToCurrency method to Product class. Use an api for getting the price dependent on a currency

--- js3w3/shoppingcart/shoppingcart.js
+++ js3w3/shoppingcart/shoppingcart.js
@@ -3,6 +3,17 @@ class Product {
         this.name = name;
         this.price = price;
     }
+
+    convertToCurrency(currency){
+        const apiKey = 'd81e4d29433f56cf29d4e576';
+        fetch(`https://prime.exchangerate-api.com/v5/${apiKey}/latest/DKK`)
+        .then(response => response.json())
+        .then(response => {
+            let convertedPrice = response.conversion_rates[currency] * this.price ;
+            console.log('The price :',this.price ,'converted to:',convertedPrice,'the currency',currency);
+            return convertedPrice 
+        });
+    }
 }
 
 class ShoppingCart {
@@ -40,15 +51,25 @@ class ShoppingCart {
         const myShoppingCart = document.querySelector('#shopping-cart');
            let shoppingCartInfo =''
            this.products.forEach(product => {
-            shoppingCartInfo = shoppingCartInfo + `<li>${product.name} </br> ${product.price}</li>`
-        })
+                            shoppingCartInfo = shoppingCartInfo + `<li>${product.name} </br>
+                            ${product.price}DKK ${product.convertToCurrency('USD')} USD </li>`
+                            })
         shoppingCartInfo = shoppingCartInfo + `<li><em>Total price </br><u> ${this.getTotal()}</u></em></li>`
-
         myShoppingCart.innerHTML =shoppingCartInfo ;
     }
 
     getUser() {
-        // Implement functionality here
+        fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then((response => response.json()))
+        .then(response => {
+         const user = document.querySelector('.user-name');
+          this.userName = response.username ;
+          this.userEmail = response.email ;
+          this.userPhone = response.phone ;
+          user.innerHTML =`Hello ${this.userName}!`;
+          console.log('user name =',this.userName,'email:',this.userEmail)
+
+        }).catch( err => console.log(err));
     }
 }
 
@@ -56,12 +77,14 @@ const shoppingCart = new ShoppingCart();
 
 const flatscreen = new Product('flat-screen', 5000);
 shoppingCart.addProduct(flatscreen);
+console.log(flatscreen.convertToCurrency('USD'));
 
 const mobile = new Product('Mobile LG', 1000);
 shoppingCart.addProduct(mobile);
 
 const bed = new Product('bed', 400);
 shoppingCart.addProduct(bed);
+// shoppingCart.removeProduct(mobile);
 
 console.log('shoppingCart: ',shoppingCart, 'Total price :',shoppingCart.getTotal());
 shoppingCart.renderProducts();
